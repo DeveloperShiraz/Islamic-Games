4d90c1f0cd338fb9ce6337be4a0576eecc1a42b2
Mohammed Shiraz <lightningboy78@gmail.com>
2025-09-15 20:04:53 +0000

    can you order the events in alphabetical order in this sport-details.ts

--- src/lib/sport-details.ts
+++ src/lib/sport-details.ts
@@ -6,15 +6,17 @@ export const sportDetails: Record<(typeof sports)[number], string> = {
       <li>Track 100, 200, 400, 800, & Mile</li>
       <li>5k RUN FOR SUDAN!</li>
       <li>Male & Female all ages</li>
-      <li>Registration: $55</li>
+      <li>Registration: $55 / Per Individual valid up to five events</li>
     </ul>
     `,
-  'Track Running': `
+  Archery: '',
+  Badminton: `
     <ul>
-      <li>Track 100, 200, 400, 800, & Mile</li>
-      <li>5k RUN FOR SUDAN!</li>
-      <li>Male & Female all ages</li>
-      <li>Registration: $55</li>
+      <li>Male & Female</li>
+      <li>Doubles only</li>
+      <li>Teams play 3 matches</li>
+      <li>Top 8 teams advance to playoffs</li>
+      <li>Registration: $55 / Per Individual valid up to five events</li>
     </ul>
     `,
   Basketball: `
@@ -23,14 +25,7 @@ export const sportDetails: Record<(typeof sports)[number], string> = {
     <br/>
     Each team plays 3 games<br/>
     Top 8 advance to Playoffs.
-    `,
-  Soccer: `
-    <b>Male:</b> Under 6 (4 vs 4), Under 9, Under 12, Under 15, Under 18 (7 vs 7), Open and 30+ (9 vs 9)<br/>
-    <b>Female:</b> Under 6 (4 vs 4), Under 9, Under 12, Under 15, 16+ (7 vs 7)<br/>
-    <ul>
-      <li>Each team plays 3 Games;</li>
-      <li>Top 8 teams advance to playoffs</li>
-    </ul>
+    <li>Registration: $55 / Per Individual valid up to five events</li>
     `,
   Cricket: `
     <ul>
@@ -39,46 +34,7 @@ export const sportDetails: Record<(typeof sports)[number], string> = {
       <li>Each team plays 3 Games</li>
       <li>Top 8 advance to playoffs</li>
       <li>Registration: $350/Team</li>
-    </ul>
-    `,
-  'Flag Football': `
-    <ul>
-      <li>Male: Open</li>
-      <li>Six players per side</li>
-      <li>Each team plays 3 Games</li>
-      <li>Top 8 advance to playoffs</li>
-    </ul>
-    `,
-  Volleyball: `
-    <ul>
-      <li>Male and Female Open Categories</li>
-      <li>Minimum 6 players per side</li>
-      <li>Each team plays 3 Games; top 8 advance to playoffs</li>
-      <li>Indoor hardwood floor</li>
-    </ul>
-    `,
-  Tennis: `
-    <ul>
-      <li>Male and Female: Youth & Adult</li>
-      <li>Singles and Doubles</li>
-      <li>Play 3 matches then playoff</li>
-      <li>Registration: $55</li>
-    </ul>
-    `,
-  Pickleball: `
-    <ul>
-      <li>Male & Female Doubles Team</li>
-      <li>Each team plays 3 games</li>
-      <li>Top 8 teams advance to playoff round</li>
-    </ul>
-    `,
-  Badminton: `
-    <ul>
-      <li>Male & Female</li>
-      <li>Doubles only</li>
-      <li>Teams play 3 matches</li>
-      <li>Top 8 teams advance to playoffs</li>
-      <li>Registration $55</li>
+      <li>Registration: $55 / Per Individual valid up to five events</li>
     </ul>
     `,
   'Fitness Course': `
@@ -93,10 +49,62 @@ export const sportDetails: Record<(typeof sports)[number], string> = {
       <li>10 Push & Pulls</li>
       <li>10 15lbs Medicine Ball Throw 10 ft</li>
       <li>10 Burpees</li>
+      <li>Registration: $55 / Per Individual valid up to five events</li>
     </ul>
     `,
-  Archery: '',
+  'Flag Football': `
+    <ul>
+      <li>Male: Open</li>
+      <li>Six players per side</li>
+      <li>Each team plays 3 Games</li>
+      <li>Top 8 advance to playoffs</li>
+      <li>Registration: $55 / Per Individual valid up to five events</li>
+    </ul>
+    `,
   'Kids Sports': '',
   'Martial Arts': '',
+  Pickleball: `
+    <ul>
+      <li>Male & Female Doubles Team</li>
+      <li>Each team plays 3 games</li>
+      <li>Top 8 teams advance to playoff round</li>
+      <li>Registration: $55 / Per Individual valid up to five events</li>
+    </ul>
+    `,
+  Soccer: `
+    <b>Male:</b> Under 6 (4 vs 4), Under 9, Under 12, Under 15, Under 18 (7 vs 7), Open and 30+ (9 vs 9)<br/>
+    <b>Female:</b> Under 6 (4 vs 4), Under 9, Under 12, Under 15, 16+ (7 vs 7)<br/>
+    <ul>
+      <li>Each team plays 3 Games;</li>
+      <li>Top 8 teams advance to playoffs</li>
+      <li>Registration: $55 / Per Individual valid up to five events</li>
+    </ul>
+    `,
   'Table Tennis': '',
+  Tennis: `
+    <ul>
+      <li>Male and Female: Youth & Adult</li>
+      <li>Singles and Doubles</li>
+      <li>Play 3 matches then playoff</li>
+      <li>Registration: $55</li>
+      <li>Registration: $55 / Per Individual valid up to five events</li>
+    </ul>
+    `,
+  'Track Running': `
+    <ul>
+      <li>Track 100, 200, 400, 800, & Mile</li>
+      <li>5k RUN FOR SUDAN!</li>
+      <li>Male & Female all ages</li>
+      <li>Registration: $55 / Per Individual valid up to five events</li>
+    </ul>
+    `,
+  Volleyball: `
+    <ul>
+      <li>Male and Female Open Categories</li>
+      <li>Minimum 6 players per side</li>
+      <li>Each team plays 3 Games; top 8 advance to playoffs</li>
+      <li>Indoor hardwood floor</li>
+      <li>Registration: $55 / Per Individual valid up to five events</li>
+    </ul>
+    `,
 };
