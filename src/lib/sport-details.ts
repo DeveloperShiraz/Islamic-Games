f3c22a2e507680a00e53337ae7cebb7515b814c6
Mohammed Shiraz <lightningboy78@gmail.com>
2025-09-15 20:20:01 +0000

    You didn't add the registration fees for every sports that was broken do

--- src/lib/sport-details.ts
+++ src/lib/sport-details.ts
@@ -10,39 +10,44 @@ export const sportDetails: Record<(typeof sports)[number], string> = {
       <li>Registration: $55/Person valid up to five events</li>
     </ul>
     `,
-  'Archery': `
+  Archery: `
     <ul>
       <li>First Aid/Medicine Fees: $5/Person</li>
       <li>Registration: $55/Person valid up to five events</li>
     </ul>
     `,
-  'Badminton': `
+  Badminton: `
     <ul>
       <li>Male & Female</li>
       <li>Doubles only</li>
       <li>Teams play 3 matches</li>
       <li>Top 8 teams advance to playoffs</li>
       <li>First Aid/Medicine Fees: $5/Person</li>
-      <li>Registration: $55/Person valid up to five events</li>
-      <li>Registration: $60/Team</li>
+      <li>Registration: $55/Person</li>
+      <li>Team Registration: $60/Team</li>
     </ul>
     `,
-  'Basketball': `
+  Basketball: `
     <b>Male:</b> Under 10, Under 13, 14 - 17 & Open (5 vs 5)<br/>
     <b>Female:</b> Under 13 and 14+ (5 vs 5)<br/>
     <br/>
     Each team plays 3 games<br/>
     Top 8 advance to Playoffs.
-    <li>Registration: $55 / Per Individual valid up to five events</li>
+    <ul>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
+      <li>Team Registration: $280/Team</li>
+    </ul>
     `,
-  'Cricket': `
+  Cricket: `
     <ul>
       <li>7 Players per side</li>
       <li>6 over per game</li>
       <li>Each team plays 3 Games</li>
       <li>Top 8 advance to playoffs</li>
-      <li>Registration: $350/Team</li>
-      <li>Registration: $55 / Per Individual valid up to five events</li>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
+      <li>Team Registration: $350/Team</li>
     </ul>
     `,
   'Fitness Course': `
@@ -57,7 +62,8 @@ export const sportDetails: Record<(typeof sports)[number], string> = {
       <li>10 Push & Pulls</li>
       <li>10 15lbs Medicine Ball Throw 10 ft</li>
       <li>10 Burpees</li>
-      <li>Registration: $55 / Per Individual valid up to five events</li>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
     </ul>
     `,
   'Flag Football': `
@@ -66,48 +72,57 @@ export const sportDetails: Record<(typeof sports)[number], string> = {
       <li>Six players per side</li>
       <li>Each team plays 3 Games</li>
       <li>Top 8 advance to playoffs</li>
-      <li>Registration: $55 / Per Individual valid up to five events</li>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
     </ul>
     `,
   'Kids Sports': `
-    </ul>
-      <li>Registration: $55 / Per Individual valid up to five events</li>
+    <ul>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
     </ul>
   `,
   'Martial Arts': `
-  </ul>
-    <li>Registration: $55 / Per Individual valid up to five events</li>
-  </ul>
-`,
-  'Pickleball': `
+    <ul>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
+    </ul>
+  `,
+  Pickleball: `
     <ul>
       <li>Male & Female Doubles Team</li>
       <li>Each team plays 3 games</li>
       <li>Top 8 teams advance to playoff round</li>
-      <li>Registration: $55 / Per Individual valid up to five events</li>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
     </ul>
     `,
-  'Soccer': `
+  Soccer: `
     <b>Male:</b> Under 6 (4 vs 4), Under 9, Under 12, Under 15, Under 18 (7 vs 7), Open and 30+ (9 vs 9)<br/>
     <b>Female:</b> Under 6 (4 vs 4), Under 9, Under 12, Under 15, 16+ (7 vs 7)<br/>
     <ul>
       <li>Each team plays 3 Games;</li>
       <li>Top 8 teams advance to playoffs</li>
-      <li>Registration: $55 / Per Individual valid up to five events</li>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
+      <li>7v7 Team Registration: $280/Team</li>
+      <li>9v9 Team Registration: $350/Team</li>
     </ul>
     `,
-  'Table Tennis':  `
-  </ul>
-    <li>Registration: $55 / Per Individual valid up to five events</li>
-  </ul>
-`,
-  'Tennis': `
+  'Table Tennis': `
+    <ul>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
+    </ul>
+  `,
+  Tennis: `
     <ul>
       <li>Male and Female: Youth & Adult</li>
       <li>Singles and Doubles</li>
       <li>Play 3 matches then playoff</li>
-      <li>Registration: $55</li>
-      <li>Registration: $55 / Per Individual valid up to five events</li>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
+      <li>Doubles Team Registration: $60/Team</li>
     </ul>
     `,
   'Track Running': `
@@ -115,16 +130,19 @@ export const sportDetails: Record<(typeof sports)[number], string> = {
       <li>Track 100, 200, 400, 800, & Mile</li>
       <li>5k RUN FOR SUDAN!</li>
       <li>Male & Female all ages</li>
-      <li>Registration: $55 / Per Individual valid up to five events</li>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
     </ul>
     `,
-  'Volleyball': `
+  Volleyball: `
     <ul>
       <li>Male and Female Open Categories</li>
       <li>Minimum 6 players per side</li>
       <li>Each team plays 3 Games; top 8 advance to playoffs</li>
       <li>Indoor hardwood floor</li>
-      <li>Registration: $55 / Per Individual valid up to five events</li>
+      <li>First Aid/Medicine Fees: $5/Person</li>
+      <li>Registration: $55/Person valid up to five events</li>
+      <li>Team Registration: $280/Team</li>
     </ul>
     `,
 };
