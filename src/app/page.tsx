0f6e14a85e974ce912998b6f96164c937c3d6df0
Mohammed Shiraz <lightningboy78@gmail.com>
2025-09-15 17:27:53 +0000

    Show this location on map "https://maps.app.goo.gl/EGQ2iX7CZDbWZ7CW8" un

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,6 +42,17 @@ export default function Home() {
               </div>
             </CardContent>
           </Card>
+          <div className="rounded-lg overflow-hidden shadow-lg">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.433599238945!2d-95.68686102368945!3d29.99625792038407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640d90d7335e955%3A0x27a5d15a9143a758!2sBridgeland%20High%20School!5e0!3m2!1sen!2sus!4v1719595561530!5m2!1sen!2sus"
+              width="100%"
+              height="450"
+              style={{ border: 0 }}
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </div>
       </div>
     </main>
